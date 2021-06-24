98ee33b890b728c6ff66d8fa16badcd5ce81cfc4
PakhninGregory <81368270+PakhninGregory@users.noreply.github.com>
2021-06-25 01:45:44 +0300

    Add files via upload

---
+++ Polar/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace Polar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Panel panel1;
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxXmin = new System.Windows.Forms.TextBox();
+            this.textBoxYmin = new System.Windows.Forms.TextBox();
+            this.textBoxXmax = new System.Windows.Forms.TextBox();
+            this.textBoxYmax = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Pakhnin_proj = new System.Windows.Forms.GroupBox();
+            panel1 = new System.Windows.Forms.Panel();
+            this.Pakhnin_proj.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new System.Drawing.Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(200, 100);
+            panel1.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X min";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Y min";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "X max";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(122, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Y max";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(507, 36);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Угол Ф";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(505, 94);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "r-радиус";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(666, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 49);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Calc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonSet_Click);
+            // 
+            // textBoxXmin
+            // 
+            this.textBoxXmin.Location = new System.Drawing.Point(49, 26);
+            this.textBoxXmin.Name = "textBoxXmin";
+            this.textBoxXmin.Size = new System.Drawing.Size(50, 20);
+            this.textBoxXmin.TabIndex = 12;
+            this.textBoxXmin.Text = "-10";
+            // 
+            // textBoxYmin
+            // 
+            this.textBoxYmin.Location = new System.Drawing.Point(49, 94);
+            this.textBoxYmin.Name = "textBoxYmin";
+            this.textBoxYmin.Size = new System.Drawing.Size(50, 20);
+            this.textBoxYmin.TabIndex = 13;
+            this.textBoxYmin.Text = "-10";
+            // 
+            // textBoxXmax
+            // 
+            this.textBoxXmax.Location = new System.Drawing.Point(163, 29);
+            this.textBoxXmax.Name = "textBoxXmax";
+            this.textBoxXmax.Size = new System.Drawing.Size(50, 20);
+            this.textBoxXmax.TabIndex = 14;
+            this.textBoxXmax.Text = "10";
+            // 
+            // textBoxYmax
+            // 
+            this.textBoxYmax.Location = new System.Drawing.Point(164, 92);
+            this.textBoxYmax.Name = "textBoxYmax";
+            this.textBoxYmax.Size = new System.Drawing.Size(50, 20);
+            this.textBoxYmax.TabIndex = 15;
+            this.textBoxYmax.Text = "10";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(300, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 20);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.Text = "0";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(300, 92);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(50, 20);
+            this.textBox4.TabIndex = 17;
+            this.textBox4.Text = "0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(426, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 20);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.Text = "1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(427, 89);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(50, 20);
+            this.textBox3.TabIndex = 19;
+            this.textBox3.Text = "1";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(557, 36);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(50, 20);
+            this.textBox5.TabIndex = 20;
+            this.textBox5.Text = "1";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(559, 94);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(50, 20);
+            this.textBox6.TabIndex = 21;
+            this.textBox6.Text = "1";
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 389);
+            this.panel2.TabIndex = 22;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.panel2.Resize += new System.EventHandler(this.panel2_Resize);
+            // 
+            // Pakhnin_proj
+            // 
+            this.Pakhnin_proj.Controls.Add(this.label1);
+            this.Pakhnin_proj.Controls.Add(this.button1);
+            this.Pakhnin_proj.Controls.Add(this.textBox6);
+            this.Pakhnin_proj.Controls.Add(this.textBoxXmin);
+            this.Pakhnin_proj.Controls.Add(this.textBox5);
+            this.Pakhnin_proj.Controls.Add(this.label10);
+            this.Pakhnin_proj.Controls.Add(this.label2);
+            this.Pakhnin_proj.Controls.Add(this.textBox3);
+            this.Pakhnin_proj.Controls.Add(this.textBoxYmin);
+            this.Pakhnin_proj.Controls.Add(this.label9);
+            this.Pakhnin_proj.Controls.Add(this.textBox1);
+            this.Pakhnin_proj.Controls.Add(this.label3);
+            this.Pakhnin_proj.Controls.Add(this.textBox4);
+            this.Pakhnin_proj.Controls.Add(this.label8);
+            this.Pakhnin_proj.Controls.Add(this.textBoxXmax);
+            this.Pakhnin_proj.Controls.Add(this.textBox2);
+            this.Pakhnin_proj.Controls.Add(this.label4);
+            this.Pakhnin_proj.Controls.Add(this.label7);
+            this.Pakhnin_proj.Controls.Add(this.textBoxYmax);
+            this.Pakhnin_proj.Controls.Add(this.label5);
+            this.Pakhnin_proj.Controls.Add(this.label6);
+            this.Pakhnin_proj.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Pakhnin_proj.Location = new System.Drawing.Point(0, 389);
+            this.Pakhnin_proj.Name = "Pakhnin_proj";
+            this.Pakhnin_proj.Size = new System.Drawing.Size(800, 141);
+            this.Pakhnin_proj.TabIndex = 23;
+            this.Pakhnin_proj.TabStop = false;
+            this.Pakhnin_proj.Text = "Pakhnin_proj";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 800);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(panel1);
+            this.Controls.Add(this.Pakhnin_proj);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "GUU Polarka";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Pakhnin_proj.ResumeLayout(false);
+            this.Pakhnin_proj.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxXmin;
+        private System.Windows.Forms.TextBox textBoxYmin;
+        private System.Windows.Forms.TextBox textBoxXmax;
+        private System.Windows.Forms.TextBox textBoxYmax;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox Pakhnin_proj;
+    }
+}
+
---
+++ Polar/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Polar
+{
+    public partial class Form1 : Form
+    {
+        private int xMin = -10;
+        private int xMax = 10;
+        private int yMin = -10;
+        private int yMax = 10;
+
+        private int dx, dy;
+        private int x0, y0;
+
+        private float x11, x12, y11, y12;
+        private int x1, x2, y1, y2;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int arrowSize = 7;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSet_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBoxXmin.Text, out xMin)) MessageBox.Show("Некорректное значение X min");
+            if (!int.TryParse(textBoxXmax.Text, out xMax)) MessageBox.Show("Некорректное значение X max");
+            if (!int.TryParse(textBoxYmin.Text, out yMin)) MessageBox.Show("Некорректное значение Y min");
+            if (!int.TryParse(textBoxYmax.Text, out yMax)) MessageBox.Show("Некорректное значение Y max");
+            if (!int.TryParse(textBox2.Text, out x1)) MessageBox.Show("Некорректное значение X1");
+            if (!int.TryParse(textBox4.Text, out y1)) MessageBox.Show("Некорректное значение Y1");
+            if (!int.TryParse(textBox1.Text, out x2)) MessageBox.Show("Некорректное значение X2");
+            if (!int.TryParse(textBox3.Text, out y2)) MessageBox.Show("Некорректное значение Y2");
+            panel2.Refresh();
+        }
+
+
+        private void panel2_Resize(object sender, EventArgs e)
+        {
+            panel2.Refresh();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            dx = panel2.Width / (xMax - xMin);
+            dy = panel2.Height / (yMax - yMin);
+            x0 = -dx * xMin;
+            y0 = dy * yMax;
+
+            for (int x = xMin; x <= xMax; x++)
+                e.Graphics.DrawLine(Pens.LightGray, x0 + x * dx, 0, x0 + x * dx, panel2.Height);
+            
+
+            for (int y = yMin; y <= yMax; y++)
+                e.Graphics.DrawLine(Pens.LightGray, 0, y0 - y * dy, panel2.Width, y0 - y * dy);
+
+            double xp, yp, F, r, pi, p, sh;
+            int o;
+
+            o = 2;
+            pi = 3.1415926535;
+            r = 0;
+
+            double x11, x12 = 0, y11, y12 = 0;
+
+            sh = (o * pi) / (o * 180);
+
+            for (int i = 0; i < o * 180; i++)
+            {
+                r += sh;
+                F = i;
+
+                xp = r * Math.Cos(F * Math.PI / 180);
+                yp = r * Math.Sin(F * Math.PI / 180);
+
+                x11 = x12;
+                y11 = y12;
+                x12 = xp;
+                y12 = yp;
+
+                e.Graphics.DrawLine(Pens.Crimson, (float)(x0 + x11 * dx), (float)(y0 - y11 * dy), (float)(x0 + x12 * dx), (float)(y0 - y12 * dy));
+            }
+
+
+            F = double.Parse(textBox5.Text);
+            r = double.Parse(textBox6.Text);
+            xp = r * Math.Cos(F * Math.PI / 180);
+            yp = r * Math.Sin(F * Math.PI / 180);
+
+            e.Graphics.DrawLine(Pens.Crimson, x0 + x1 * dx, y0 - y1 * dy, (float)(x0 + xp * dx), (float)(y0 - yp * dy));
+            e.Graphics.DrawLine(Pens.Black, 0, y0, panel2.Width, y0);
+            e.Graphics.DrawLine(Pens.Black, x0, 0, x0, panel2.Height);
+
+            e.Graphics.FillPolygon(Brushes.Black, new PointF[] { new PointF(panel2.Width, y0), new PointF(panel2.Width - arrowSize, y0 - arrowSize), new PointF(panel2.Width - arrowSize, y0 + arrowSize) });
+            e.Graphics.FillPolygon(Brushes.Black, new PointF[] { new PointF(x0, 0), new PointF(x0 - arrowSize, arrowSize), new PointF(x0 + arrowSize, arrowSize) });
+
+            Font f = new Font("Arial", 12);
+            e.Graphics.DrawString("Y", f, Brushes.Black, x0 + 10, 0);
+            e.Graphics.DrawString("X", f, Brushes.Black, panel2.Width - 25, y0 + 10);
+        }
+    }
+}
